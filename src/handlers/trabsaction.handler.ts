52b31c8b1f91e03f101eaee07c018e9518dc7b35
Ahmed Ragab <ahmedasiawy@gmail.com>
2022-05-14 04:49:24 +0200

    Add: Transaction handler.

---
+++ src/handlers/trabsaction.handler.ts
@@ -0,0 +1,58 @@
+import { Request, Response, Application } from "express";
+
+import { Op, col } from "sequelize";
+
+import { BadTransaction, HttpStatus } from "../util/enums";
+import { Transaction } from "../models/Transaction";
+import { Duration } from "../util/types";
+
+async function index(req: Request, res: Response): Promise<void> {
+	const perPage = Number(req.query.per_page);
+	const { page, seller_id, date_range } = req.query;
+
+	const offset = Number(page) * perPage - perPage;
+	const totalTransactions = offset + perPage;
+
+	try {
+		const duration = Transaction.dateFromDuration(date_range as Duration);
+
+		const transactions = await Transaction.findAll({
+			order: col("id"),
+			offset,
+			limit: perPage,
+			attributes: ["id", "title", "image", "price", ["updatedAt", "last_updated"]],
+			where: {
+				sellerId: Number(seller_id),
+				updatedAt: {
+					[Op.between]: duration,
+				},
+			},
+		});
+
+		const paging = {
+			total: totalTransactions,
+			current_page: page,
+			per_page: perPage,
+		};
+		const result_transaction = {
+			data: {
+				transactions: transactions,
+				paging,
+			},
+		};
+
+		res.status(HttpStatus.ok).json(result_transaction);
+	} catch (err) {
+		res.status(HttpStatus.badReq).json({
+			error: {
+				status: BadTransaction.status,
+				message: BadTransaction.allTransactions,
+			},
+		});
+		console.log(err);
+	}
+}
+
+export const transaction_routes = (app: Application): void => {
+	app.get("/transactions", index);
+};
