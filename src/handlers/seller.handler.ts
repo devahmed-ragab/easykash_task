a229f75e3adbed82e5cfdea22f60a4400c1af2fa
Ahmed Ragab <ahmedasiawy@gmail.com>
2022-05-14 15:23:40 +0200

    Fix: seller.handler quiry.

--- src/handlers/seller.handler.ts
+++ src/handlers/seller.handler.ts
@@ -1,11 +1,11 @@
 import { Request, Response, Application } from "express";
-import { Op, col, fn } from "sequelize";
+import { QueryTypes } from "sequelize";
 
-import { Seller } from "../models/Seller";
 import { Duration } from "../util/types";
 import { dateFromDuration } from "../util/utill";
-import { Transaction } from "../models/Transaction";
 import { BadSeller, HttpStatus } from "../util/enums";
+
+import { sequelize } from "../sequelize";
 import auth from "../middlewares/authentication";
 
 async function index(req: Request, res: Response): Promise<void> {
@@ -14,33 +14,32 @@ async function index(req: Request, res: Response): Promise<void> {
 
 	try {
 		const duration = dateFromDuration(dateRange as Duration);
-		console.log("-------------", duration);
-		// { group: 'name' }
-		// group: [sequelize.fn('date_trunc', 'day', sequelize.col('createdAt'))]
-		const sellerSummary = await Seller.findOne({
-			attributes: ["seller_name", "seller_id"],
-			where: {
-				sellerId: Number(sellerId),
-				include: [
-					{
-						model: Transaction,
-						attributes: [
-							["updatedAt", "date"],
-							[fn("SUM", col("price")), "total_income"],
-						],
-						where: {
-							updatedAt: {
-								[Op.between]: duration,
-							},
-						},
-					},
-				],
+
+		const quiry = `
+					select 
+						s.name as seller_name,
+						s.id as seller_id,
+						Date(t.updatedAt) as date,
+						sum(price) as total_income
+					from transactions t
+					join sellers s
+					on s.id = t.sellerId
+						where s.id = :sellerId and
+						      t.updatedAt BETWEEN :startDate and :endDate
+					group by Date(t.updatedAt), s.name, s.id;`;
+
+		const result = await sequelize.query(quiry, {
+			replacements: {
+				sellerId: sellerId,
+				startDate: duration[0],
+				endDate: duration[1],
 			},
+			type: QueryTypes.SELECT,
 		});
 
-		res.status(HttpStatus.ok).json(sellerSummary);
+		res.status(HttpStatus.ok).json({ data: { days: result } });
 	} catch (err) {
-		res.status(HttpStatus.badReq).json({
+		res.status(HttpStatus.serverError).json({
 			error: {
 				status: BadSeller.status,
 				message: BadSeller.allTransactions,
